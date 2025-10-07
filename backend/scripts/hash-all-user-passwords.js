96da94dfb0e3be669d09b3eba7eb8404b2a72411
VermaMasum <masum.mobilions@gmail.com>
2025-10-07 17:45:14 +0530

    07/10

--- backend/scripts/hash-all-user-passwords.js
+++ backend/scripts/hash-all-user-passwords.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("./models/User"); // path to your User model
+const User = require("../models/User"); // path to your User model
 
 // Connect to MongoDB
 mongoose
